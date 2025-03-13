917c2fff1a70e6c7efcca57146b5c6ae85932c3e
Dani8448 <dalx8448@gmail.com>
2025-03-13 19:16:52 +0100

    evento mosueover y out

---
+++ js/eventos.js
@@ -0,0 +1,7 @@
+document.getElementById("miBoton").addEventListener("mouseover", function() {
+    this.style.backgroundColor = "Green";  // Cambia el color al pasar el cursor
+});
+
+document.getElementById("miBoton").addEventListener("mouseout", function() {
+    this.style.backgroundColor = "skyblue";  
+});
