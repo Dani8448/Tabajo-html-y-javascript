e63c8baa2b5daa7b34bcc43da7d07efb9b002da2
Dani8448 <dalx8448@gmail.com>
2025-03-13 19:30:55 +0100

    Update eventos.js

--- js/eventos.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("miBoton").addEventListener("mouseover", function() {
-    this.style.backgroundColor = "Green";  // Cambia el color al pasar el cursor
-});
-
-document.getElementById("miBoton").addEventListener("mouseout", function() {
-    this.style.backgroundColor = "skyblue";  
-});
