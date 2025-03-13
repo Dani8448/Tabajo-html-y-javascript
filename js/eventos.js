b8839d471c2cf8ce7d921ad7c3537309b5cfb76c
Dani8448 <dalx8448@gmail.com>
2025-03-13 21:05:24 +0100

    dinamismo 1

--- js/eventos.js
+++ js/eventos.js
@@ -1,12 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let formulario = document.getElementById("miFormulario");
-    let campoTexto = document.getElementById("campoTexto");
+    let formulario = document.querySelector("form"); 
+    let inputs = formulario.querySelectorAll("input, textarea"); 
 
-    campoTexto.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") { 
-            event.preventDefault();  // Evita el envío automático del formulario
-            alert("Formulario enviado con Enter: " + campoTexto.value);
-            formulario.submit();  // Envía el formulario manualmente
-        }
+    inputs.forEach(input => {
+        input.addEventListener("keydown", function(event) {
+            if (event.key === "Enter") { 
+                event.preventDefault();  
+                alert("Formulario enviado con Enter");
+                formulario.submit();  
+            }
+        });
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let btnVerde = document.getElementById("fondoVerde");
+    let btnAzul = document.getElementById("fondoAzul");
+    let btnReset = document.getElementById("resetFondo");
+
+    
+    btnVerde.addEventListener("click", function() {
+        document.body.style.backgroundColor = "green";
+    });
+
+    btnAzul.addEventListener("click", function() {
+        document.body.style.backgroundColor = "blue";
+    });
+
+    btnReset.addEventListener("click", function() {
+        document.body.style.backgroundColor = ""; 
     });
 });
